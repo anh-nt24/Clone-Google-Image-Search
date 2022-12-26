1945b77f9f75f182ab439209355264bc760ac528
anh-nt24 <theanh24013@gmail.com>
2022-12-26 11:50:44 +0700

    first commit

---
+++ cbir.py
@@ -0,0 +1,29 @@
+from feature_vector import *
+
+train = False
+if train:
+    train()
+
+img_path = 'Database/test/image_00200.jpg'
+
+model = get_model()
+s = get_feature_vector(model, img_path)
+v = pickle.load(open(vector_file, 'rb'))
+p = pickle.load(open(path_file, 'rb'))
+
+d = np.linalg.norm(s-v, axis=1)
+w = 10
+idx = np.argsort(d)[:w] # the smaller the distance is, the more the same they are 
+neighbor = dict({(p[i], d[i]) for i in idx})
+imgs = [Image.open(i) for i in neighbor.keys()]
+
+plt.imshow(Image.open(img_path))
+plt.title('Queried Image')
+dis = [*neighbor.values()]
+fig = plt.figure(figsize=(15,7))
+ax = fig.subplots(2,5)
+for x in range(len(neighbor)):
+    ax.flatten()[x].imshow(imgs[x])
+    ax.flatten()[x].title.set_text(f'Độ giống: {round((1-dis[x])*100,2)}%')
+    ax.flatten()[x].axis('off')
+plt.show()
---
+++ feature_vector.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import os
+#import tensorflow as tf
+#from keras.models import Model 
+#from keras.utils import img_to_array # convert output to vector
+import pickle
+
+def get_model():
+    mobile = tf.keras.applications.mobilenet.MobileNet(weights='imagenet')
+    model = Model(inputs=mobile.inputs, outputs=mobile.get_layer('reshape_2').output)
+    return model
+
+def preprocessing(img):
+    img = img.resize((224,224))
+    img = img.convert('RGB')
+    img = img_to_array(img)
+    img = np.expand_dims(img,0)
+    img = tf.keras.applications.mobilenet.preprocess_input(img)
+    return img
+
+def get_feature_vector(model, path):
+    img = Image.open(path)
+    ts = preprocessing(img)
+    vector = model.predict(ts)[0]
+    vector = vector/np.linalg.norm(vector)
+    return vector
+
+base_dir = '.'
+vector_file = 'Database/feature.pkl'
+path_file = 'Database/path.pkl'
+db_dir = os.path.join(base_dir, 'images')
+def train():
+    vectors = []
+    paths = []
+    model = get_model()
+    for i in os.listdir(db_dir):
+        img_path = os.path.join(db_dir, i)
+        vectors.append(get_feature_vector(model, img_path))
+        paths.append(img_path)
+    
+    pickle.dump(vectors, open(vector_file, 'wb'))
+    pickle.dump(paths, open(path_file, 'wb'))
